dae5e5c6713a1f12fa835f2b9d184773307491b1
Дrake <59538985+Flawed-8230@users.noreply.github.com>
2022-05-17 19:44:19 -0400

    Update/Fix Radio.js
    +Previous Track Bug resolved
    +New Station CSC added
    +Credits Section

--- radio.js
+++ radio.js
@@ -28,6 +28,8 @@ function random(min, max) {
 }
 
 function playaudio(url, radstat) {
+    prev = 0
+    next = 0
     var audio = document.createElement('audio');
     curr = 1
     clink = url
@@ -37,9 +39,9 @@ function playaudio(url, radstat) {
     audio.autoplay = true;
     document.getElementById("h1").innerHTML = `Now Playing: ${radstat}`
     setInterval(function() {
-    if(clos == 1) {audio.remove();curr = 0};
-    if(next == 1) {next = 0; audio.remove();curr = 0;}
-    if(prev == 1) {audio.remove();curr = 0; prev = 0}
+    if(clos == 1) {audio.remove();curr = 0;plink = url; pname = radstat;};
+    if(next == 1) {next = 0; audio.remove();curr = 0;plink = url; pname = radstat;}
+    if(prev == 1) {audio.remove();curr = 0}
     },1000)
     audio.onended = function(){
         audio.remove()
@@ -67,8 +69,9 @@ OWOP.windowSys.addWindow(new OWOP.windowSys.class.window("Sajvnczeid Radio!", {
     <button id="PV">Previous</button>
     <button id="NX">Skip</button>
     <button id="LP">Loop</button>
-    <button id="OFF">Off</button><br>
-    <h id="h1">Now Playing: ${radstat}</h></div>`; //add extra buttons and eventlisteners to make more stations!
+    <button id="OFF">Off</button><span>'              '</span>
+    <button id="C">Credits</button><br>
+    <h2 id="h1">Now Playing: ${radstat}</h2></div>`; //add extra buttons and eventlisteners to make more stations!
     
   }).move(0, 0));
 
@@ -80,11 +83,11 @@ clos = 0
 document.getElementById("ODR").addEventListener("click", () => {
     playing = 2
     clos = 0
-});;
+});
 
 document.getElementById("NX").addEventListener("click", () => {
     next = 1
-});;
+});
 
 document.getElementById("PV").addEventListener("click", () => {
     prev = 1
@@ -99,7 +102,21 @@ document.getElementById("LP").addEventListener("click", () => {
 document.getElementById("OFF").addEventListener("click", () => {
     clos = 1
     playing = 0
-});;
+});
+
+document.getElementById("C").addEventListener("click", () => {
+    Alert('Radio Developed by Drakevskiy/Flawed#8230 with help from Enix')
+    OWOP.windowSys.addWindow(new OWOP.windowSys.class.window("Sajvnczeid Radio!", {
+    closeable: true,
+    moveable: true
+  }, h => {
+    WINDOW = h;
+    h.container.style.color = "white";
+    h.container.innerHTML = `
+    <img src="https://files.catbox.moe/nyik8r.png" alt="If you see this message contact D. Sajvnczeid#8230" width="500" height="525"><br>
+    <span>'Radio Developed by Drakevskiy/Flawed#8230 with help from Enix'</span>`
+  }).move(0, 0));
+});
 
 setInterval(function() {
 if(curr == 1) return
@@ -115,12 +132,10 @@ switch (playing) {
         j = random(0, stations.odrnames.length - 1)
         playaudio(stations.odrlinks[j], stations.odrnames[j])
         break;
-    /*
-    case etc-number:
-        j = random(0, stations.customnames.length - 1)
-        playaudio(stations.customlinks[j])
+    case 3:
+        j = random(0, stations.csrnames.length - 1)
+        playaudio(stations.csrlinks[j])
         break;
-    */
     default:
         break;
 }
